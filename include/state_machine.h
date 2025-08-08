b559578413460a8c0f716786f56cc6b68f8fcbf9
Atharva Pardeshi <atharva.exe@gmail.com>
2025-08-08 14:15:03 +0530

    feat: include sdcard library, accept sdcard object as argument

--- include/state_machine.h
+++ include/state_machine.h
@@ -7,12 +7,14 @@
 #include "dht11_driver.h"
 #include "gps_driver.h"
 #include "mpu6050_driver.h"
+#include "sdcard_driver.h"
 
 enum FlightState { PRELAUNCH, ASCENT, DESCENT, POSTLAND };
 
 void stateMachineInit(BMP280_Driver &bmp, DHT11_Driver &dht,
                       MPU6050_Driver &mpu, Compass_Driver &compass,
-                      GPS_Driver &gps, Buzzer_Driver &buzzer);
+                      GPS_Driver &gps, Buzzer_Driver &buzzer,
+                      SDCard_Driver &sdcard);
 void stateMachineUpdate();
 
 #endif // !STATE_MACHINE_H
